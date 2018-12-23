ea0bea03bbec776ed1702bc511979d217ece69c8
npriakos <44199887+npriakos@users.noreply.github.com>
2018-12-23 18:11:09 +0200

    Add files via upload

--- omadiki_ergasia_java_2/Alternate.java
+++ omadiki_ergasia_java_2/Alternate.java
@@ -1,22 +1,29 @@
-package java2;
+package omadiki_ergasia_java_2;
 
 import java.util.ArrayList;
-
 public class Alternate {
 
-	public void deleteTable(ArrayList<ArrayList<String>>arl) {
-		arl.clear();
-	}
-	public void deleteRows(ArrayList<ArrayList<String>>arl,int x) {
-		arl.remove(x);
-	}
-	public static void updateValues(ArrayList<ArrayList<String>>arl,int x,ArrayList<String>) {
-		for(int i=0; i=arl.lenght(); i++) {
-			if (i==x) {
-				arl.remove(x);
-				set(x,ArrayList(String));
-			}
-		}
-	}
+    public void deleteTable(ArrayList<ArrayList<String>> arl) {
+        arl.clear();
+    }
 
+    public static void deleteRows(ArrayList<ArrayList<String>> arl, int x) {
+        arl.remove(x);
+    }
+
+    public static void updateValues(int x, ArrayList<ArrayList<String>> array, ArrayList<String> values ) {
+       
+        for(int i = 0; i < array.size(); i++){
+            for (int j = 0; j < array.get(i).size(); j++) {
+                if(i==x){
+                    array.remove(x);
+                    array.add(x, values);
+                }
+            }
+        }
+    }
+    
+    public static void antimetathesi(){
+        
+    }
 }
