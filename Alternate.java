f1f0b6178518fe1b2e8679a642afe37828faca22
Giannis Kokkosis <gkokkosis1998@gmail.com>
2018-12-23 20:56:51 +0200

    Add files via upload

---
+++ Alternate.java
@@ -0,0 +1,33 @@
+package omadiki_ergasia_java_2;
+
+import java.util.ArrayList;
+public class Alternate {
+
+    public void deleteTable(ArrayList<ArrayList<String>> arl) {
+        arl.clear();
+    }
+
+    public static void deleteRows(ArrayList<ArrayList<String>> arl, int x, String values) {
+        arl.remove(x);
+    }
+    // klisi antikeimenou Create table
+    public static void updateValues(int x,CreateTable table ) {
+       
+        for(int i = 0; i < table.getValues().size(); i++){
+            for (int j = 0; j < table.getValues().size(); j++){
+                if(i==x){
+                    table.getValues().get(x).remove(x);
+                    //table.getValues().add(x, values); edw vgazei error
+                }
+            }
+        }
+    }
+    
+    public static void antimetathesi(){
+        
+    }
+    
+    public void a (CreateTable table){
+        table.getValues().size();
+    }
+}
---
+++ SearchRetriveData.java
@@ -0,0 +1,24 @@
+package omadiki_ergasia_java_2;
+
+import java.util.ArrayList;
+
+public class SearchRetriveData {
+
+    // klisi antikeimenou Create table
+    public ArrayList<String> searchData(CreateTable table, String value) {//Vazw ton pinaka san orisma gia na kserei panta h methodos se poion pinaka na psaksei
+        ArrayList<String> array = new ArrayList<>();
+        for (int i = 0; i < table.getValues().size(); i++) {//prospelasi stis times tou pinaka
+            for (int j = 0; j < table.getValues().size(); j++) {
+                if (value.equals(table.getValues().get(i).get(j))) {//sinthiki pou an ali8euei epistrefei olokliri tin pleiada
+                    array = table.getValues().get(i);//epistrefei olokliri tin pleiada 
+                }
+            }
+        }
+        return array;
+    }
+
+    public static String getMessage() {
+        return "No values found";
+    }// methodos pou xrisimopoieitai gia na epistrefei to antisoixo minima ston xristi ean den vrei kamia antisoixisi ston pinaka. Pio polu ti vazw gia na min epistrefei sketo null
+
+}
