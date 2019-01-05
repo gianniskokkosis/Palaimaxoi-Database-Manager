e81a654025af10c856d3eb273e7472cc6bf1fc05
Philip_Pap <philippapakonstantinou@gmail.com>
2019-01-06 01:28:18 +0200

    Add 'dataTypes' ArrayList to constructor along with set and get methods

--- omadiki_ergasia_java_2/CreateTable.java
+++ omadiki_ergasia_java_2/CreateTable.java
@@ -1,40 +1,46 @@
 package omadiki_ergasia_java_2;
 
-import java.util.Scanner;
 import java.util.ArrayList;
 
 public class CreateTable {
-
-    private String tableName;
-    private int numberOfColumns;
-    private ArrayList<String> columnNames;
+	private String tableName;
+	private int numberOfColumns;
+	private ArrayList<String> columnNames;
+	private ArrayList<Integer> dataTypes;
     private ArrayList<ArrayList<String>> values;
 
-    //Constructor
-    public CreateTable(String tableName, int numberOfColumns, ArrayList<String> columnNames) {
-        this.tableName = tableName;
+  	//Constructor
+	public CreateTable(String tableName, int numberOfColumns, ArrayList<String> columnNames,
+    	ArrayList<Integer> dataTypes) {
+
+    	this.tableName = tableName;
         this.numberOfColumns = numberOfColumns;
         this.columnNames = columnNames;
+        this.dataTypes = dataTypes;
         this.values = new ArrayList<ArrayList<String>>();
         this.values.add(columnNames);
     }
 
     //setters
     public void setTableName(String name) {
-        this.tableName = name;
+    	this.tableName = name;
     }
 
     public void setNumberOfColumns(int number) {
-        this.numberOfColumns = number;
+    	this.numberOfColumns = number;
     }
 
     public void setColumnNames(ArrayList<String> names) {
-        this.columnNames = names;
+    	this.columnNames = names;
     }
+
+    public void setDataTypes(ArrayList<Integer> dataTypes) {
+		this.dataTypes = dataTypes;
+	}//end of setters
 
     //getters
     public String getName() {
-        return ("  " + this.tableName + "\n");
+        return ("  " + this.tableName + "");
     }
 
     public int getNumberOfColumns() {
@@ -49,53 +55,17 @@ public class CreateTable {
         return (this.values);
     }
 
+    public ArrayList<Integer> getDataTypes() {
+		return this.dataTypes;
+	}//end of getters
+
     //Adds a row of values to the table
     public void addRow(ArrayList<String> row) {
-        ArrayList<String> newRow = new ArrayList<String>();
+    	ArrayList<String> newRow = new ArrayList<String>();
 
         for (int i = 0; i < row.size(); i++) {
             newRow.add(row.get(i));
         }
-
-        this.values.add(newRow);
+    	this.values.add(newRow);
     }
-
-    public static void main(String args[]) {
-
-        String name = "Agenda";
-        int number = 5;
-        ArrayList<String> columns = new ArrayList<String>();
-
-        columns.add("SURNAME");
-        columns.add("FIRSTNAME");
-        columns.add("PHONE");
-        columns.add("EMAIL");
-        columns.add("ADDRESS");
-
-        ArrayList<ArrayList<String>> values = new ArrayList<ArrayList<String>>();
-        CreateTable table = new CreateTable(name, number, columns);
-        Scanner input = new Scanner(System.in);
-        ArrayList<String> pleiada = new ArrayList<String>();
-
-        pleiada.add("Papak");
-        pleiada.add("Philip");
-        pleiada.add("6984104787");
-        pleiada.add("philipapakonstantinou@gamil.com");
-        pleiada.add("Somewhere");
-
-        table.addRow(pleiada);
-
-        pleiada.clear();
-
-        System.out.println();
-
-        pleiada.add("Xatz");
-        pleiada.add("Vas");
-        pleiada.add("6984104787");
-        pleiada.add("xatz@gamil.com");
-        pleiada.add("Somewhere else");
-        table.addRow(pleiada);
-        pleiada.clear();
-
-    }// END OF MAIN
 }
