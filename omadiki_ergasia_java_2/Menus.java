14a1a9a070c395527a4ad95c4a6333a77f785764
Philip_Pap <philippapakonstantinou@gmail.com>
2019-01-06 01:38:05 +0200

    Update class 'Menus' to utilize the 'InputChecks' class

--- omadiki_ergasia_java_2/Menus.java
+++ omadiki_ergasia_java_2/Menus.java
@@ -1,53 +1,57 @@
 package omadiki_ergasia_java_2;
 
+import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
-
 
 public class Menus {
 	private static Scanner input = new Scanner(System.in);
 
+	// Main menu of Database
 	public static int starterMenu() {
 		String userInput;
 		int menuInput = 0;
 
+		System.out.println();
+		System.out.println(" --- MENU ---");
+		System.out.println(" 1.Create a Table");
+		System.out.println(" 2.Insert data");
+		System.out.println(" 3.View data");
+		System.out.println(" 4.Search for Data");
+		System.out.println(" 5.Update Data");
+		System.out.println(" 6.Delete Data");
+		System.out.println(" 7.Exit");
+
 		while ( (menuInput != 1 && menuInput != 2 && menuInput != 3 && menuInput != 4 && menuInput != 5
 			&& menuInput != 6 && menuInput != 7 )) {
-				System.out.println();
-				System.out.println(" --- MENU ---");
-				System.out.println("  1.Create a Table");
-				System.out.println("  2.Insert data");
-				System.out.println("  3.View data");
-				System.out.println("  4.Search for Data");
-				System.out.println("  5.Update Data");
-				System.out.println("  6.Delete Data");
-				System.out.println("  7.Exit");
-				System.out.print("  Please type one of the above numbers. Your choice: ");
-				userInput = input.nextLine();
 
-				try {
-					menuInput = Integer.parseInt(userInput);
-				} catch (NumberFormatException e) {
-					menuInput = -1;
-					System.out.print(" Invalid input. Please try again.\n");
-				}
+			System.out.print("Please type one of the above numbers. Your choice: ");
+			userInput = input.nextLine();
+
+			try {
+				menuInput = Integer.parseInt(userInput);
+			} catch (NumberFormatException e) {
+				menuInput = -1;
+				System.out.print("Invalid input. Please try again.\n");
 			}
-			return menuInput;
+		}
+		return menuInput;
 	}
 
-	//Create Table menu
+	//Create a Table menu
 	public static CreateTable menuCreateTable() {
 		String wordEnding;
 		String userInput;
 		String tableName = null;
 		int numberOfColumns = 0;
 		ArrayList<String> columns = new ArrayList<String>();
+		ArrayList<Integer> dataTypes = new ArrayList<Integer>();
 
 		while ( numberOfColumns <= 0 || (tableName == null || tableName.equals(""))  ) {
 			System.out.println();
-			System.out.println("  Please type the name of the Table.");
+			System.out.println("Please type the name of the Table.");
 			tableName = input.nextLine();
-			System.out.println("  Please type the number of columns the table should contain.\n  ");
+			System.out.println("Please type the number of columns the table should contain.");
 			userInput = input.nextLine();
 
 			try {
@@ -67,25 +71,34 @@ public class Menus {
 				wordEnding = "th";
 			}
 
-			System.out.println("  Please type the name of the " + i + wordEnding +
-				" column: \n  ");
+			System.out.println("Please type the name of the " + i + wordEnding +
+				" column: ");
 			String nameOfColumns = input.nextLine();
 			columns.add(nameOfColumns);
+			System.out.println("\n---DATA DEFINITION MENU ---");
+			System.out.println("\n1.Varchar (string/text)");
+			System.out.println("2.Integer");
+			System.out.println("3.Real (double)");
+			System.out.println("4.Date (simple format: dd/MM/yyyy)");
+
+			int dataChoice = InputChecks.checkDataDefinitonInput(nameOfColumns);
+			dataTypes.add(dataChoice);
 		}
-		CreateTable table = new CreateTable(tableName, numberOfColumns, columns);
-		System.out.println("  Your table has been created and is ready to accept data\n");
+		CreateTable table = new CreateTable(tableName, numberOfColumns, columns, dataTypes);
+		System.out.println("Your table has been successfully created and is ready to accept data\n");
 		return table;
 	}
 
 	//The insert data menu
-	public static void insertRowsMenu(CreateTable table) {
+	public static void InsertRowsMenu(CreateTable table) {
 		int numberOfRows = 0;
-		String userInput;
+		String userInput = "";
+		boolean  isValid;
 		ArrayList<String> userAddedRow = new ArrayList<String>();
 
 		while(numberOfRows <= 0) {
 			System.out.println();
-			System.out.println("  Please type the number of rows you wish to add: ");
+			System.out.println("Please type the number of rows you wish to add: ");
 			userInput = input.nextLine();
 
 			try {
@@ -94,18 +107,27 @@ public class Menus {
 				numberOfRows = -1;
 			}
 		}
+
 		for (int i = 1; i <= numberOfRows; i++) {
-			System.out.println("  Please type values for the " + i + " row \n");
+			System.out.println("Please type values for the " + i + " row \n");
 			for(int j = 0; j < table.getNumberOfColumns(); j++) {
-				System.out.println("    Type a value for the field " + "'" +
-					table.getColumnNames().get(j) + "'" + " : ");
+				isValid = false;
+				while (isValid == false ) {
+					System.out.println("Type a value for the field " + "'" +
+						table.getColumnNames().get(j) + "'" + ": ");
 
 					userInput = input.nextLine();
-					userAddedRow.add(userInput);
+
+					isValid = InputChecks.checkDataInput(table.getDataTypes().get(j),
+						userInput);
+				}
+				userAddedRow.add(userInput);
 			}
 			table.addRow(userAddedRow);
 			userAddedRow.clear();
 		}
+
+		System.out.println("Your data has been successfully inserted.");
 	}
 
 	//The view data menu
@@ -118,11 +140,11 @@ public class Menus {
 
 		while(choice <= 0) {
 			System.out.println();
-			System.out.println("  --- VIEW DATA MENU ---");
-			System.out.println("  1.Print the entire table");
-			System.out.println("  2.Print specific rows ");
-			System.out.println("  3.Back to start");
-			System.out.print(" Please type one of the above numbers. Your choice: ");
+			System.out.println("--- VIEW DATA MENU ---");
+			System.out.println("1.Print the entire table");
+			System.out.println("2.Print specific rows ");
+			System.out.println("3.Back to start");
+			System.out.print("Please type one of the above numbers. Your choice: ");
 
 			input1 = input.nextLine();
 
@@ -138,14 +160,10 @@ public class Menus {
 				SearchRetrieveData.printTable(table);
 				break;
 			case 2:
-				System.out.println("\nPlease type two integers to define a range(e.g. 2 and 4). The rows of the table within the defined" +
-				"range will be printed on the screen");
-				input1 = input.nextLine();
-				input2 = input.nextLine();
+				while ( (int1 <= 0) || (int2 <= 0) || (int1 > int2)) {
 
-				while ( (int1 <= -1) || (int2 <= -1) || (int1 > int2) ) {
-					System.out.println("  Please type two integers to define a range(e.g. 2 and 4). The rows of the table within the defined" +
-					 	" range will be printed on the screen. \n ATTENTION!!! The first integer should be lesser or equal to the second.");
+					System.out.println("\nPlease type two integers to define a range(e.g. 2 and 4). The rows of the table within the defined" +
+					 	" range will be printed on the screen.\nATTENTION!!! The first integer should be lesser or equal to the second.\n");
 					input1 = input.nextLine();
 					input2 = input.nextLine();
 
@@ -157,6 +175,9 @@ public class Menus {
 						int1 = -1;
 						int2 = -1;
 					}
+					if( (int1 >= table.getValues().size()) || (int2 >= table.getValues().size()) ) {
+						System.out.println("The table does not contain so many rows. It only contains: " + (table.getValues().size()-1));
+					}
 				}
 
 				SearchRetrieveData.printRows(table,int1,int2);
@@ -166,32 +187,41 @@ public class Menus {
 		}
 	}
 
-	//The search Menu
+	// The search Menu
 	public static String searchMenu() {
-		System.out.println(" Please type the value you wish to search for.");
+		System.out.println("Please type the value you wish to search for.");
 		String value = input.nextLine();
 		return value;
 	}
 
 	//The update menu
 	public static void updateMenu(CreateTable table) {
+		int chosenRow = 0;
+		String userInput;
+		boolean isValid;
 		ArrayList<String> update = new ArrayList<String>();
 
-		System.out.println("  We reccomend viewing the contents of the table before selecting " +
-			"which rows to update ");
-		System.out.println("  Choose a row to update: " );
-		int chosenRow = input.nextInt();
-		String inputS;
+		System.out.println("Choose a row to update: " );
+		userInput = input.nextLine();
 
 
-		for(int i = 0; i< table.getColumnNames().size() - 1; i++) {
-			System.out.println("  Type a value for the field " + "'" +
-				table.getColumnNames().get(i) + "'" + " : ");
-			inputS = input.nextLine();
-			update.add(inputS);
+		chosenRow = InputChecks.checkTableSize(table.getValues().size(), userInput);
+
+
+		for(int i = 0; i< table.getColumnNames().size(); i++) {
+			isValid = false;
+			while (isValid == false ) {
+				System.out.println("Type a value for the field " + "'" +
+					table.getColumnNames().get(i) + "'" + " : ");
+				userInput = input.nextLine();
+
+				isValid = InputChecks.checkDataInput(table.getDataTypes().get(i), userInput);
+			}
+			update.add(userInput);
 		}
 		TableProcessing.updateValues(table,chosenRow,update);
-		System.out.println(" Your data has been succesfully updated");
+		update.clear();
+		System.out.println("Your data has been succesfully updated");
 	}
 
 	//The delete menu
@@ -200,10 +230,10 @@ public class Menus {
 
 		while( choice <= 0) {
 			System.out.println("  --- DELETE MENU ---");
-			System.out.println("  1.Delete Table");
-			System.out.println("  2.Delete Rows");
-			System.out.println("  3.Back to start");
-			System.out.print("  Your choice: ");
+			System.out.println("1.Delete Table");
+			System.out.println("2.Delete Rows");
+			System.out.println("3.Back to start");
+			System.out.print("Your choice: ");
 			String userInput = input.nextLine();
 
 			try {
@@ -221,7 +251,7 @@ public class Menus {
 				String input1;
 				String input2;
 
-				System.out.println("  Please type two integers to define a range(e.g. 2 and 4). The rows of the table within the defined" +
+				System.out.println("Please type two integers to define a range(e.g. 2 and 4). The rows of the table within the defined" +
 					" range will be deleted.");
 
 				input1 = input.nextLine();
@@ -236,7 +266,7 @@ public class Menus {
 					int2 = -1;
 				}
 				while ( (int1 == -1) || (int2 == -1) || (int1 > int2) ) {
-					System.out.println("  Please type two integers to define a range(e.g. 2 and 4). The rows of the table within the defined" +
+					System.out.println("Please type two integers to define a range(e.g. 2 and 4). The rows of the table within the defined" +
 					 	" range will be deleted. \n ATTENTION!!! The first integer should be lesser or equal to the second.");
 					input1 = input.nextLine();
 					input2 = input.nextLine();
