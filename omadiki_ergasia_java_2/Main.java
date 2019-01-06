a3eb5c55cf0162e0e5611470d834234efc21775d
Philip_Pap <philippapakonstantinou@gmail.com>
2019-01-06 20:53:55 +0200

    Update 'Main' class

--- omadiki_ergasia_java_2/Main.java
+++ omadiki_ergasia_java_2/Main.java
@@ -1,28 +1,64 @@
 package omadiki_ergasia_java_2;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Main{
-    
-    public static void main(String[]args){
-        SearchRetriveData srd = new SearchRetriveData();
-        TableProcessing tblp = new TableProcessing();
-        Scanner input = new Scanner(System.in);
-        int choice = input.nextInt();
-        
-        int a = Menus.starterMenu();
+public class Main {
 
-        if (a==1) {
-           CreateTable table = Menus.menuCreateTable();
-        }
-        if (a==2){
-            Menus.menu_Search_Retrive_Data();
-        }
-        if (a==3){
-            Menus.menu_Table_Processing();
-        }
-        if(a==4){
-            System.out.println("You have exist the palaimaxoi data base. We appreciate your preference!");
-        }   
-    }
+	private static CreateTable table = null;
+	private static String userInput;
+	private	static int menuInput = 0;
+	private static ArrayList<String> pleiada = new ArrayList<String>();
+
+    public static void main(String[] args){
+		System.out.println(" Welcome to the 'Palaimaxoi' Database Manager ");
+		while (menuInput != 7) {
+			menuInput = Menus.starterMenu();
+
+			switch (menuInput) {
+				case 1:
+					table = Menus.menuCreateTable();
+					break;
+				case 2:
+					if (table == null) {
+						System.out.println("\nNo table was found! Please create a table first.\n");
+					} else {
+						Menus.InsertRowsMenu(table);
+					}
+					break;
+				case 3:
+					if (table == null) {
+						System.out.println("\nNo table was found! Please create a table first.\n");
+					} else {
+						Menus.viewMenu(table);
+					}
+					break;
+				case 4:
+					if (table == null) {
+						System.out.println("\nNo table was found! Please create a table first.\n");
+					} else {
+						userInput = Menus.searchMenu();
+						SearchRetrieveData.searchData(table,userInput);
+					}
+					break;
+				case 5:
+					if (table == null) {
+						System.out.println("\nNo table was found! Please create a table first.\n");
+					} else {
+						Menus.updateMenu(table);
+					}
+					break;
+				case 6:
+					if (table == null) {
+						System.out.println("\nNo table was found! Please create a table first.\n");
+					} else {
+						Menus.deleteMenu(table);
+					}
+					break;
+				case 7:
+					break;
+			}
+		}
+    	System.out.println("\nThank you for using our database manager. We appreciate your preference!\n");
+	}
 }
